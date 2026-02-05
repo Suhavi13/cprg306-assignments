efbeea3a3aa19d2dfeccc3420241084eb6927113
Suhavi13 <suhavikaur@edu.sait.ca>
2026-02-04 21:28:10 -0700

    add itemlist compoenent and page function

---
+++ app/week-4/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+
+export default function Page(){
+    return(
+        <main>
+            <div>
+                <h1>Shopping List</h1>
+                <ItemList />
+            </div>
+        </main>
+    );
+}
