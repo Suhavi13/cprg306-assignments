50be0956a5ac92d9eb92242988b86d460fe42aa2
Suhavi13 <suhavikaur@edu.sait.ca>
2026-03-22 20:23:51 -0600

    page.js link

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -31,7 +31,7 @@ export default function Home() {
           <p>
             Welcome, {user.displayName} ({user.email})
           </p>
-          <Link href="app\week-9\shopping-list\page.js">Go to Shopping List</Link>
+          <Link href="/week-9/shopping-list">Go to Shopping List</Link>
           <br />
           <button onClick={handleSignOut}>Log Out</button>
         </div>
